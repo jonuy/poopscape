6311bf2146339bfb537ae3c2c327ddd9bb23fa98
jon uy <jonxuy@gmail.com>
2015-11-26 14:41:22 -0500

    More seed info for reviews and users

--- server/seed.js
+++ server/seed.js
@@ -1,15 +1,22 @@
 var seeder = require('mongoose-seed');
 var locationData = require('./seed/locations.seed.js');
-console.log(locationData);
+var reviewData = require('./seed/reviews.seed.js');
+var userData = require('./seed/users.seed.js');
 
 seeder.connect('mongodb://localhost/poopscape', function() {
   seeder.loadModels([
       'locations/models/Location.js',
+      'reviews/models/Review.js',
+      'users/models/User.js'
     ]);
 
   seeder.clearModels([
-      'Location'
+      'Location',
+      'Review',
+      'User'
     ], function() {
       seeder.populateModels(locationData);
+      seeder.populateModels(reviewData);
+      seeder.populateModels(userData);
     });
 });
--- server/seed/locations.seed.js
+++ server/seed/locations.seed.js
@@ -1,5 +1,8 @@
+var mongoose = require('mongoose');
+
 var data = [
   {
+    _id: mongoose.Types.ObjectId('locseed00000'),
     name: 'Dunkin Donuts',
     street_address: '361 1st Avenue',
     city_address: 'New York',
@@ -9,9 +12,14 @@ var data = [
     loc: {
       type: 'Point',
       coordinates: [-73.9796427, 40.7357309]
-    }
+    },
+    reviews: [
+      mongoose.Types.ObjectId('rvwseed00000'),
+      mongoose.Types.ObjectId('rvwseed00001')
+    ]
   },
   {
+    _id: mongoose.Types.ObjectId('locseed00001'),
     name: 'Dunkin Donuts',
     street_address: '266 1st Avenue',
     city_address: 'New York',
@@ -21,9 +29,13 @@ var data = [
     loc: {
       type: 'Point',
       coordinates: [-73.9809114, 40.7315461]
-    }
+    },
+    reviews: [
+      mongoose.Types.ObjectId('rvwseed00002')
+    ]
   },
   {
+    _id: mongoose.Types.ObjectId('locseed00002'),
     name: 'Dunkin Donuts',
     street_address: '542 E 14th St',
     city_address: 'New York',
@@ -36,6 +48,7 @@ var data = [
     }
   },
   {
+    _id: mongoose.Types.ObjectId('locseed00003'),
     name: 'Dunkin Donuts',
     street_address: '215 1st Avenue',
     city_address: 'New York',
@@ -48,6 +61,7 @@ var data = [
     }
   },
   {
+    _id: mongoose.Types.ObjectId('locseed00004'),
     name: 'Starbucks',
     street_address: '400 E 23rd St',
     city_address: 'New York',
@@ -57,9 +71,14 @@ var data = [
     loc: {
       type: 'Point',
       coordinates: [-73.987364, 40.740301]
-    }
+    },
+    reviews: [
+      mongoose.Types.ObjectId('rvwseed00003'),
+      mongoose.Types.ObjectId('rvwseed00004')
+    ]
   },
   {
+    _id: mongoose.Types.ObjectId('locseed00005'),
     name: 'Starbucks',
     street_address: '286 1st Avenue',
     city_address: 'New York',
@@ -72,6 +91,7 @@ var data = [
     }
   },
   {
+    _id: mongoose.Types.ObjectId('locseed00006'),
     name: 'Starbucks',
     street_address: '219 1st Avenue',
     city_address: 'New York',
@@ -84,6 +104,7 @@ var data = [
     }
   },
   {
+    _id: mongoose.Types.ObjectId('locseed00007'),
     name: 'Starbucks',
     street_address: '145 2nd Avenue',
     city_address: 'New York',
